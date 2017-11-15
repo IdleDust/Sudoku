519076296ea82befe8da2463c96c050d379c060e
Juan Chen <juanchen917@gmail.com>
2017-11-15 15:08:38 -0800

    worker thread finished

---
+++ src/SudokuValidator.java
@@ -0,0 +1,81 @@
+/**
+ * Created by juanchen on 11/15/17.
+ */
+
+import java.util.Arrays;
+
+public class SudokuValidator {
+    private static int[][] newGrid = { {9, 1, 2, 3, 4, 5, 6, 7, 8},
+            {1,2,3,4,5,6,7,8,9},
+            {3,2,5,6,5,7,8,9,1},
+            {2,3,4,5,6,8,7,9,1},
+            {9,5,4,6,7,8,9,1,2},
+            {3,2,5,6,5,7,8,9,1},
+            {3,2,5,6,5,7,8,9,1},
+            {3,2,5,6,5,7,8,9,1},
+            {3,2,5,6,5,7,8,9,1}};
+
+    public static void main(String[] args) {
+
+        GridDemo newDemo = new GridDemo(newGrid);
+        newDemo.printGrid();
+        for (int i = 0; i < 3*9; i++) {
+            Thread t;
+            if (i < 9) {
+                t = new Thread(new WorkerThread("row", i, newDemo));
+            }
+            else if ( i < 18) {
+                t = new Thread(new WorkerThread("col", i-9, newDemo));
+            }
+            else {
+                t = new Thread(new WorkerThread("sub", i-18, newDemo));
+            }
+            t.start();
+        }
+        System.out.println("After: ");
+        newDemo.printGrid();
+        System.out.println("Finished all threads");
+    }
+
+}
+
+class GridDemo {
+    int[][] grid;
+    int row[];
+    int col[];
+    int sub[];
+
+    GridDemo(int[][] newGrid) {
+        grid = new int[9][9];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                grid[i][j] = newGrid[i][j];
+            }
+        }
+        row = new int[9];
+        Arrays.fill(row, -1);
+        col = new int[9];
+        Arrays.fill(col, -1);
+        sub = new int[9];
+        Arrays.fill(sub, -1);
+    }
+
+
+    void printGrid() {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.print("row: ");
+        for(int i = 0; i < 9; i++) System.out.print(row[i] + " ");
+        System.out.println();
+        System.out.print("col: ");
+        for(int i = 0; i < 9; i++) System.out.print(col[i] + " ");
+        System.out.println();
+        System.out.print("sub: ");
+        for(int i = 0; i < 9; i++) System.out.print(sub[i] + " ");
+        System.out.println();
+    }
+}
---
+++ src/WorkerThread.java
@@ -0,0 +1,75 @@
+import java.util.HashSet;
+
+/**
+ * Created by juanchen on 11/15/17.
+ */
+public class WorkerThread implements Runnable{
+
+    GridDemo myGridDemo;
+    private String command;
+    private int idx;
+
+    public WorkerThread(String s, int index, GridDemo newGD){
+        this.command = s;
+        this.idx = index;
+        this.myGridDemo = newGD;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName()+" Start. Command = "+ command);
+        processCommand();
+        System.out.println(Thread.currentThread().getName()+" End.");
+    }
+
+    private void processCommand() {
+
+        if (command.equals("row")) myGridDemo.row[idx] = processRowOrCol();
+        else if (command.equals("col")) myGridDemo.col[idx] = processRowOrCol();
+        else if (command.equals("sub")) myGridDemo.sub[idx] = processSub();
+    }
+
+    private int processRowOrCol() {
+        HashSet hs = new HashSet();
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            int cur;
+            if(command == "row") cur = myGridDemo.grid[idx][i];
+            else cur = myGridDemo.grid[i][idx];
+            if (cur <= 0 || cur > 9) return 0;
+            if (hs.contains(cur)) return 0;
+            else {
+                hs.add(cur);
+                sum += cur;
+            }
+        }
+        if (sum == 45) return 1;
+        else return 0;
+    }
+
+    private int processSub() {
+        HashSet hs = new HashSet();
+        int sum = 0;
+        int[][] sub_index= {{0,0}, {0,3},{0,6},{3,0},{3,3},{3,6},{6,0},{6,3},{6,6}};
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                int di = sub_index[idx][0] + i;
+                int dj = sub_index[idx][1] + j;
+                int cur = myGridDemo.grid[di][dj];
+                if (cur <= 0 || cur > 9) return 0;
+                if (hs.contains(cur)) return 0;
+                else {
+                    hs.add(cur);
+                    sum += cur;
+                }
+            }
+        }
+        if (sum == 45) return 1;
+        else return 0;
+    }
+
+    @Override
+    public String toString(){
+        return this.command;
+    }
+}
